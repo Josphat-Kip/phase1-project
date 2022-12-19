29dcade613d8ebaf980b5c3dde970fe1dd0c41e3
Josphat-Kip <josphatkiplimo247@gmail.com>
2022-12-19 11:46:40 +0300

    COntnuing

---
+++ app.js
@@ -0,0 +1,10 @@
+// const appId= "76373897&app"
+// const appKey="df1f360db504337e63b125276c33e88f"
+// const baseUrl= `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appId}$app_key=${appKey}`;
+// function loadRecipies(Type="Paneer"){
+//   const url=baseUrl+`&q=${type}`;
+//   fetch(url)
+//   .then(response=>response.json())
+//   then(data=> console.log(data.hits))
+//   .catch((error) =>console.log(error));
+// }
